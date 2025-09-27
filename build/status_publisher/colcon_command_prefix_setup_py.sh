36df8ea6c2279369f6d529af50b96ebd64be3a71
phostau628 <468965968@qq.com>
2025-09-27 16:10:12 +0800

    优化

--- build/status_publisher/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/phostau/computer_condition_display/install/status_interfaces/share/status_interfaces/package.sh"
--- build/status_publisher/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/phostau/computer_condition_display/install/status_publisher'
